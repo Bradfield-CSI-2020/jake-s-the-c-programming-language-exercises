a3ad0e3caf4eac5aae393c49ad2138f94c74f332
Jake Shasteen <jakeshasteen@gmail.com>
2020-07-10 20:25:26 -0700

    Add version of binsearch that adds more tests outside the loop than inside

--- 3-1/binsearch.c
+++ 3-1/binsearch.c
@@ -23,6 +23,25 @@ int binsearch(int x, int v[], int n) {
   return -1; /* no match */
 }
 
+/* binsearch2: find x in v[0] <= v[1] <= ... <= v[n-1] */
+int binsearch2(int x, int v[], int n) {
+  int low, mid, high;
+
+  low = 0;
+  high = n - 1;
+  while (low <= high) {
+    mid = (low+high) / 2;
+    if (x < v[mid]) { /* target is to the left */
+      high = mid - 1;
+    } else { /* target is to the right */
+      low = mid + 1;
+    }
+  }
+  if(x == v[mid]) return mid;
+  if(x == v[mid+1]) return mid+1;
+  if(x == v[mid-1]) return mid-1;
+  return -1; /* no match */
+}
 
 // ./binsearch target el0 el1 el2 el3 ... elN
 int main(int argc, char *argv[]) {
@@ -31,6 +50,6 @@ int main(int argc, char *argv[]) {
   for(int i = 2; i < argc; i++) {
     numsv[i-2] = atoi(argv[i]);
   }
-  printf("%d\n", binsearch(atoi(argv[1]), numsv, numsc));
+  printf("%d\n", binsearch2(atoi(argv[1]), numsv, numsc));
   return 0;
 }
