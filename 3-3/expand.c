a475f830fb9eacab07dcc638feb44854de41d46a
Jake Shasteen <jakeshasteen@gmail.com>
2020-07-11 12:32:31 -0700

    Add expand function and tests

--- 3-3/expand.c
+++ 3-3/expand.c
@@ -1,3 +1,6 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
 /*
   Write a function expand(s1, s2) that expands shorthand notations
   like a-z in the string s1 into the equivalent complete list abc...xyz
@@ -6,10 +9,42 @@
   taken literally.
 */
 
+char bothAreDigits(char c1, char c2) {
+  return isdigit(c1) && isdigit(c2);
+}
+
+char bothAreAlphaAndLower(char c1, char c2) {
+  return isalpha(c1) && isalpha(c2) && islower(c1) && islower(c2);
+}
+
+char bothAreAlphaAndNotLower(char c1, char c2) {
+  return isalpha(c1) && isalpha(c2) && !islower(c1) && !islower(c2);
+}
+
 void expand(char s1[], char s2[]) {
-  
+  int s1len = strlen(s1);
+  int i, j;
+  int count = 0;
+  for(i = 0, j = 0; i < s1len; i++) {
+    if(s1[i] == '-'
+        && i > 0
+        && i < s1len-1
+        && (bothAreDigits(s1[i-1], s1[i+1]) || bothAreAlphaAndLower(s1[i-1], s1[i+1]) || bothAreAlphaAndNotLower(s1[i-1], s1[i+1]))) {
+      j--; /* backtrack */
+      for(char k = s1[i-1]; k <= s1[i+1]; k++, j++) {
+        s2[j] = k;
+      }
+      i++; /* skip the next character */
+    } else {
+      s2[j] = s1[i];
+      j++;
+    }
+  }
 }
 
 int main(int argc, char *argv[]) {
+  char result[200] = "";
+  expand(argv[1], result);
+  printf("%s\n", result);
   return 0;
 }
