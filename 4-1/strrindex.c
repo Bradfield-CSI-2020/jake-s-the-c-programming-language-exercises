d814b4438533d46c94ca87a8e94b4570d285b882
Jake Shasteen <jakeshasteen@gmail.com>
2020-07-11 23:28:26 -0700

    Touch strrindex.c

---
+++ 4-1/strrindex.c
@@ -0,0 +1,13 @@
+/*
+  Write the function strrindex(s,t) which returns the position
+  of the rightmost occurrence of t in s, or -1 if there is none.
+*/
+
+
+int strrindex(char s[], char t) {
+  
+}
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
