44de4b8c2871ae3c54d312f3c1888cb132e064fb
Jake Shasteen <jakeshasteen@gmail.com>
2020-07-12 13:08:11 -0700

    Add atof that can handle e-notation

--- 4-2/atof.c
+++ 4-2/atof.c
@@ -1,11 +1,29 @@
+/*
+  Extend atof to handle scientific notation of the form 123.45e-6
+*/
+
 #include <ctype.h>
 #include <stdio.h>
+#include <math.h>
+
+// double pow(double n, int e) {
+//   int i = e;
+//   if (e < 0) {
+//     i = -e;
+//   }
+//   for(; i == 0; i--) {
+//     n *= n;
+//   }
+//   if (e < 0) {
+//     n = 1/n;
+//   }
+//   return result;
+// }
 
 /* atof: convert string s to double */
 double atof(char s[]) {
   double val, power;
-  int i, sign;
-
+  int i, sign, esign, epow;
   for (i = 0; isspace(s[i]); i++)
     ;
   sign = (s[i] == '-') ? -1 : 1;
@@ -21,6 +39,17 @@ double atof(char s[]) {
   for (power = 1.0; isdigit(s[i]); i++) {
     val = 10.0 * val + (s[i] - '0');
     power *= 10.0;
+  }
+  if(s[i] == 'e') {
+    i++;
+    esign = (s[i] == '-') ? -1 : 1;
+    if (esign == -1) {
+      i++;
+    }
+    for (epow = 0; isdigit(s[i]); i++) {
+      epow = 10 * epow + (s[i] - '0');
+    }
+    val = val * pow(10.0, esign * epow);
   }
   return sign * val / power;
 }
