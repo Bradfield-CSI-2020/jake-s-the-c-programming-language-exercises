c529bea50142ff40bc933f4a807a28b55d0fefbf
Jake Shasteen <jakeshasteen@gmail.com>
2020-07-16 11:29:06 -0700

    Transcribe readlines

---
+++ 5-7/readlines.c
@@ -0,0 +1,55 @@
+/*  Exercise 5-7
+    Rewrite readlines to store lines in an array supplied by main, 
+    rather than calling alloc to maintain storage. How much faster
+    is the program?
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000
+
+char *lineptr[MAXLINES];
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+
+int main() {
+  int nlines;
+
+  if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+    qsort(lineptr, 0, nlines-1);
+    writelines(lineptr, nlines);
+    return 0;
+  } else {
+    printf("error: input too big to sort \n");
+    return 1;
+  }
+}
+
+#define MAXLEN 1000
+int getline(char *, int);
+char *alloc(int);
+
+int readlines(char *lineptr[], int maxlines) {
+  int len, nlines;
+  char *p, line[MAXLEN];
+
+  nlines = 0;
+  while((len = getline(line, MAXLEN)) > 0) {
+    if (nlines >= maxlines || (p = alloc(len)) == NULL) {
+      return -1;
+    } else {
+      line[len-1] = 0;
+      strcpy(p, line);
+      lineptr[nlines++] = p;
+    }
+  }
+  return nlines;
+}
+
+void writelines(char *lineptr[], int nlines) {
+  while(nlines--) {
+    printf("%s\n", *lineptr++);
+  }
+}
