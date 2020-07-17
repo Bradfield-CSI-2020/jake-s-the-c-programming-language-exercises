6bd8da5a82324051c22bb1033b23ffa3c4b038fb
Jake Shasteen <jakeshasteen@gmail.com>
2020-07-16 22:03:28 -0700

    Expose month_day and day_of_month via main

--- 5-8/daytab.c
+++ 5-8/daytab.c
@@ -1,6 +1,8 @@
 /*  Exercise 5-8
     There is no error checking in day_of_year or month_day. Remedy this defect.
 */
+#include <stdio.h>
+#include <stdlib.h>
 
 static char daytab[2][13] = {
   {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
@@ -11,7 +13,7 @@ static char daytab[2][13] = {
 int day_of_year(int year, int month, int day) {
   int i, leap;
 
-  leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+  leap = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
   for (i = 1; i < month; i++) {
     day += daytab[leap][i];
   }
@@ -22,7 +24,7 @@ int day_of_year(int year, int month, int day) {
 void month_day(int year, int yearday, int *pmonth, int *pday) {
   int i, leap;
 
-  leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+  leap = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
   for (i = 1; yearday > daytab[leap][i]; i++) {
     yearday -= daytab[leap][i];
   }
@@ -31,6 +33,10 @@ void month_day(int year, int yearday, int *pmonth, int *pday) {
 }
 
 int main(int argc, char *argv[]) {
-  
+  int month;
+  int day;
+
+  month_day(atoi(argv[1]), atoi(argv[4]), &month, &day);
+  printf("%d %d %d\n", day_of_year(atoi(argv[1]), atoi(argv[2]), atoi(argv[3])), month, day);
   return 0;
 }
