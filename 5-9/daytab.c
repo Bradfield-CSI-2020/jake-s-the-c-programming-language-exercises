1e258d4111f27900003b23f4b590d1c448242186
Jake Shasteen <jakeshasteen@gmail.com>
2020-07-17 18:21:23 -0700

    Convert daytab functions to pointers

--- 5-9/daytab.c
+++ 5-9/daytab.c
@@ -11,39 +11,41 @@ static char daytab[2][13] = {
 
 /* day_of_year: set day of year from month & day */
 int day_of_year(int year, int month, int day) {
-  int i, leap;
-
+  int leap;
 
   leap = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
-  if (month > 12 || month < 1 || day > *(*(daytab + leap) + month)) return -1;
+  char *pdaytab = daytab[leap];
 
-  for (i = 1; i < month; i++) {
-    day += *(*(daytab + leap) + i);
+  if (month > 12 || month < 1 || day > *(pdaytab + month)) return -1;
+
+  for (; month > 0; month--) {
+    day += *pdaytab++;
   }
   return day;
 }
 
 /* month_day: set month, day from day of year */
 void month_day(int year, int yearday, int *pmonth, int *pday) {
-  int i, leap;
+  int leap;
 
   leap = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+  char *pdaytab = daytab[leap];
+
   if(yearday > (leap ? 366 : 365) || yearday < 1) {
     *pmonth = -1;
     *pday = -1;
     return;
   }
 
-  for (i = 1; yearday > *(*(daytab + leap) + i); i++) {
-    yearday -= *(*(daytab + leap) + i);
+  for (; yearday > *pdaytab;) {
+    yearday -= *pdaytab++;
   }
-  *pmonth = i;
+  *pmonth = pdaytab - daytab[leap]; /* end - start = length. Equiv to 'i'. */
   *pday = yearday;
 }
 
 int main(int argc, char *argv[]) {
-  int month;
-  int day;
+  int month, day;
 
   month_day(atoi(argv[1]), atoi(argv[4]), &month, &day);
   printf("%d %d %d\n", day_of_year(atoi(argv[1]), atoi(argv[2]), atoi(argv[3])), month, day);
