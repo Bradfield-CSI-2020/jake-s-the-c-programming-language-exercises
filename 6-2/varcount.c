ffbe840a5d381b58e641e3d702cc984e9662caa4
Jake Shasteen <jakeshasteen@gmail.com>
2020-07-19 14:02:05 -0700

    Describe varcount.c

---
+++ 6-2/varcount.c
@@ -0,0 +1,7 @@
+/*  Exercise 6-2
+    Write a C program that reads a C program and prints in alphabetical order each group
+    of variable names that are identical in the first N characters, but different somewhere
+    thereafter. Don't count words within strings and comments. Make N a parameter that can
+    be set from the command line.
+*/
+
